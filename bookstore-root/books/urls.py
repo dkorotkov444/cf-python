739968aedb843366ca7748b40e14f954f665913f
DK-CF <korotkovadvisory@gmail.com>
2026-02-18 19:01:22 +0100

    A2-E2.5 Created vies & templates, registered them - for books app.

---
+++ bookstore-root/books/urls.py
@@ -0,0 +1,9 @@
+# books/urls.py
+from django.urls import path
+from .views import books
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', books, name='books'),
+]
--- bookstore-root/books/views.py
+++ bookstore-root/books/views.py
@@ -1,3 +1,6 @@
+# books/views.py
 from django.shortcuts import render
 
 # Create your views here.
+def books(request):
+    return render(request, 'books/books.html')
