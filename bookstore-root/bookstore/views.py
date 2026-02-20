ce03f920e466a6203b887e501a440841b5318519
DK-CF <korotkovadvisory@gmail.com>
2026-02-20 18:07:42 +0100

    A2-E2.6 Defined function-based view for lofin request for Bookstore application.

---
+++ bookstore-root/bookstore/views.py
@@ -0,0 +1,31 @@
+# bookstore/views.py
+
+from django.shortcuts import render, redirect               # Django functions for rendering templates and redirecting
+from django.contrib.auth import authenticate, login         # Django authentication libraries   
+from django.contrib.auth.forms import AuthenticationForm    # Django Form for authentication
+
+# Function-based view for handling login requests
+def login_view(request):            
+    form = AuthenticationForm()     # Create an instance of the AuthenticationForm class
+
+    if request.method == 'POST':        # Check if the request method is POST
+        error_message = None            # Default error message for invalid login
+        form = AuthenticationForm(request, data=request.POST)       # Create an instance of the AuthenticationForm class with the POST data
+        if form.is_valid():                                         # Check if the form is valid
+            username = form.cleaned_data.get('username')            # Get the username from the cleaned data
+            password = form.cleaned_data.get('password')            # Get the password from the cleaned data
+            user = authenticate(username=username, password=password)   # Authenticate the user
+            if user is not None:                                    # i.e. the user is authenticated
+                login(request, user)                                # Use pre-defined Django function to login
+                return redirect('sales:records')                    # Redirect to a success page.
+        else:
+            error_message = 'Invalid username or password.'     # Set error message for invalid login
+    
+    # Prepare data to send from view to template
+    context ={                                             
+        'form': form,                               # send the form data
+        'error_message': error_message              # and the error_message
+    }
+
+    # Load the login page using "context" information
+    return render(request, 'auth/login.html', context) 
