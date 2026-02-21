fdadc0d9770e8f9a48f99a910cf50abd9fb5a5b8
DK-CF <korotkovadvisory@gmail.com>
2026-02-21 08:57:48 +0100

    A2-E2.6 Created and registered logout view for Bookstore application.

--- bookstore-root/bookstore/urls.py
+++ bookstore-root/bookstore/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import login_view
+from .views import login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
     path('', include('sales.urls')),
     path('books/', include('books.urls')),
     path('customers/', include('customers.urls')),
--- bookstore-root/bookstore/views.py
+++ bookstore-root/bookstore/views.py
@@ -1,8 +1,8 @@
 # bookstore/views.py
 
-from django.shortcuts import render, redirect               # Django functions for rendering templates and redirecting
-from django.contrib.auth import authenticate, login         # Django authentication libraries   
-from django.contrib.auth.forms import AuthenticationForm    # Django Form for authentication
+from django.shortcuts import render, redirect                       # Django functions for rendering templates and redirecting
+from django.contrib.auth import authenticate, login, logout         # Django authentication libraries   
+from django.contrib.auth.forms import AuthenticationForm            # Django Form for authentication
 
 # Function-based view for handling login requests
 def login_view(request):  
@@ -10,14 +10,14 @@ def login_view(request):
     form = AuthenticationForm()     # Create an instance of the AuthenticationForm class
 
     if request.method == 'POST':        # Check if the request method is POST
-        form = AuthenticationForm(request, data=request.POST)       # Create an instance of the AuthenticationForm class with the POST data
-        if form.is_valid():                                         # Check if the form is valid
-            username = form.cleaned_data.get('username')            # Get the username from the cleaned data
-            password = form.cleaned_data.get('password')            # Get the password from the cleaned data
+        form = AuthenticationForm(request, data=request.POST)           # Create an instance of the AuthenticationForm class with the POST data
+        if form.is_valid():                                             # Check if the form is valid
+            username = form.cleaned_data.get('username')                # Get the username from the cleaned data
+            password = form.cleaned_data.get('password')                # Get the password from the cleaned data
             user = authenticate(username=username, password=password)   # Authenticate the user
-            if user is not None:                                    # i.e. the user is authenticated
-                login(request, user)                                # Use pre-defined Django function to login
-                return redirect('sales:records')                    # Redirect to a success page.
+            if user is not None:                                        # i.e. the user is authenticated
+                login(request, user)                                    # Use pre-defined Django function to login
+                return redirect('sales:records')                        # Redirect to a success page.
         else:
             error_message = 'Invalid username or password.'     # Set error message for invalid login
     
@@ -28,4 +28,9 @@ def login_view(request):
     }
 
     # Load the login page using "context" information
-    return render(request, 'auth/login.html', context) 
+    return render(request, 'auth/login.html', context)
+
+# Function-based view for handling logout requests
+def logout_view(request):
+    logout(request)                             # Use pre-defined Django function to logout
+    return redirect('login')                    # Redirect to a success page.
