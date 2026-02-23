cbd11355c8e80e5f6fa4202c4da9b4b4e9551ca1
DK-CF <korotkovadvisory@gmail.com>
2026-02-23 16:40:16 +0100

    A2-E2.7 Developed views, utility functions and template to display sales charts.

--- bookstore-root/sales/utils.py
+++ bookstore-root/sales/utils.py
@@ -1,7 +1,9 @@
 # sales/utils.py
 # This file contains utility functions for the sales app.
 # It is used to define functions that can be reused across the sales app.
-
+from io import BytesIO 
+import base64
+import matplotlib.pyplot as plt
 from books.models import Book
 
 # Function retrieved book title by ID
@@ -10,3 +12,72 @@ def get_booktitle_from_id(val):
         return Book.objects.get(id=val).title
     except Book.DoesNotExist:
         return "Unknown Book"
+
+# Function to convert a matplotlib graph to a format that can be rendered in HTML templates
+def get_graph():
+   buffer = BytesIO()       # Create a BytesIO buffer for the image         
+   # Create a plot with a bytesIO object as a file-like object. Set format to png
+   plt.savefig(buffer, format='png')
+   buffer.seek(0)       # Set cursor to the beginning of the stream
+
+   image_png=buffer.getvalue()          # Retrieve the content of the file
+   graph=base64.b64encode(image_png)    # Encode the bytes-like object
+   graph=graph.decode('utf-8')          # Decode to get the string as output
+
+   buffer.close()       # Free up the memory of buffer
+   return graph         # Return the graph in a format that can be rendered in HTML templates
+
+# Function to generate different types of charts based on user input
+#       - chart_type: user input,
+#       - data: pandas dataframe
+def get_chart(chart_type, data, **kwargs):
+    # Switch plot backend to AGG (Anti-Grain Geometry) - to write to file
+    # AGG is preferred solution to write PNG files
+    plt.switch_backend('AGG')
+    fig=plt.figure(figsize=(6,3))        # Specify figure size
+
+    # Select chart_type based on user input from the form
+    if chart_type == '#1':
+        # Plot bar chart between date on x-axis and quantity on y-axis
+        plt.bar(data['date_created'], data['quantity'])
+
+    elif chart_type == '#2':
+        # Generate pie chart based on the price. The book titles are sent from the view as labels
+        labels=kwargs.get('labels')
+        plt.pie(data['price'], labels=labels)
+
+    elif chart_type == '#3':
+        # Plot line chart based on date on x-axis and price on y-axis
+        plt.plot(data['date_created'], data['price'])
+    else:
+        print ('Unknown chart type')
+
+    plt.tight_layout()      # Specify layout details
+    chart =get_graph()      # Render the graph to file
+    return chart  
+
+# Function to explore querysets and understand the output of different queryset methods
+def explore_querysets():
+    print ('Exploring querysets:')
+    print ('\nCase 1: Output of Sale.objects.all()')
+    qs=Sale.objects.all()
+    for item in qs:
+        print (item)
+
+    print ('\nCase 2: Output of Sale.objects.filter(book__title=book_title)')
+    qs =Sale.objects.filter(book__title=book_title)
+    for item in qs:
+        print (item)
+
+    print ('\nCase 3: Output of qs.values')
+    for item in qs.values():
+        print (item)
+
+    print ('\nCase 4: Output of qs.values_list()')
+    for item in qs.values_list():
+        print (item)
+
+    print ('\nCase 5: Output of Sale.objects.get(id=1)')
+    obj = Sale.objects.get(id=1)
+    for field in obj._meta.fields:
+        print (f'{field.name}: {getattr(obj, field.name)}')
--- bookstore-root/sales/views.py
+++ bookstore-root/sales/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 import pandas as pd
 from .models import Sale
 from .forms import SalesSearchForm
-# from .utils import get_booktitle_from_id
+from .utils import get_chart, explore_querysets
 
 
 # Create your views here.
@@ -16,11 +16,12 @@ def home(request):
 # Function-based view records()
 @login_required
 def records(request):
-    form = SalesSearchForm()        # Create an instance of the SalesSearchForm class and assign it to the variable form
     sales_df = None                 # Initialize the variable sales_df to None
+    chart = None                    # Initialize the variable chart to None
 
     # Check if the button is clicked
     if request.method =='POST':
+        form = SalesSearchForm(request.POST)        # Create an instance of the SalesSearchForm class and assign it to the variable form
         # Read book_title and chart_type
         book_title = request.POST.get('book_title')
         chart_type = request.POST.get('chart_type')
@@ -31,44 +32,25 @@ def records(request):
         qs =Sale.objects.filter(book__title=book_title)
         if qs.exists():          # If data found, convert the queryset to pandas DataFrame
             sales_df = pd.DataFrame(qs.values())
-
-            #sales_df['book_id']=sales_df['book_id'].apply(get_booktitle_from_id)     # Convert book_id to book title using the get_booktitle_from_id function
             sales_df['book_id']=book_title          # Directly overwrite book_id with book_title since we already have the book title
             # Rename 'book_id' to 'Book Title' and 'id' to 'Sale ID' (or any others)
             sales_df.rename(columns={'book_id': 'Book Title', 'id': 'Sale ID'}, inplace=True)
-
             sales_df=sales_df.to_html(index=False)             # Convert the DataFrame to HTML format for rendering in the template, removing line numbers (index=False)
 
-    # --- Explore querysets ---
-    #print ('Exploring querysets:')
-    #print ('\nCase 1: Output of Sale.objects.all()')
-    #qs=Sale.objects.all()
-    #for item in qs:
-    #    print (item)
+            # Generate chart
+            chart = get_chart(chart_type, sales_df, labels=sales_df['date_created'].values)
 
-    #print ('\nCase 2: Output of Sale.objects.filter(book__title=book_title)')
-    #qs =Sale.objects.filter(book__title=book_title)
-    #for item in qs:
-    #    print (item)
+            # --- DEBUG --- EXPOLRE QUERYSETS ---
+            explore_querysets()       # Call the function to explore querysets and understand the output of different queryset methods
 
-    #print ('\nCase 3: Output of qs.values')
-    #for item in qs.values():
-    #    print (item)
-
-    #print ('\nCase 4: Output of qs.values_list()')
-    #for item in qs.values_list():
-    #    print (item)
-
-    #print ('\nCase 5: Output of Sale.objects.get(id=1)')
-    #obj = Sale.objects.get(id=1)
-    #for field in obj._meta.fields:
-    #    print (f'{field.name}: {getattr(obj, field.name)}')
-    # -------------------------------------------------------
+    else:
+        form = SalesSearchForm()       # If the button is not clicked, create an instance of the SalesSearchForm class and assign it to the variable form
 
     # Create a context dictionary to pass the form and sales_df variables to the template
     context = {
         'form': form,
-        'sales_df': sales_df
+        'sales_df': sales_df,
+        'chart': chart
     }        
 
     # Render the 'sales/records.html' template with the context dictionary and return the resulting HttpResponse object
