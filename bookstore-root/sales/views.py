decbc8e475ec0c764208e1712c663f5dd59b2941
DK-CF <korotkovadvisory@gmail.com>
2026-02-23 11:36:11 +0100

    A2-E2.7 Created a form for searching sales record in Bookstore.Embedded it into template.

--- bookstore-root/sales/views.py
+++ bookstore-root/sales/views.py
@@ -2,6 +2,7 @@
 from urllib import request
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import SalesSearchForm
 
 # Create your views here.
 # Function-based view to display homepage
@@ -11,4 +12,7 @@ def home(request):
 # Function-based view records()
 @login_required
 def records(request):
-    return render(request, 'sales/records.html')
+    form = SalesSearchForm()        # Create an instance of the SalesSearchForm class and assign it to the variable form
+    context = {'form': form}        # Create a context dictionary with the key 'form' and the value of the form instance
+    # Render the 'sales/records.html' template with the context dictionary and return the resulting HttpResponse object
+    return render(request, 'sales/records.html', context)
